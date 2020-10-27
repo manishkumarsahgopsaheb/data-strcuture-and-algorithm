3742703eee76856488aca1893ae0a34ffc69397f
Manish Kumar Sah <47948821+manishkumarsahgopsaheb@users.noreply.github.com>
2020-10-27 22:10:51 +0530

    Update structure.c

--- structure.c
+++ structure.c
@@ -1,228 +1,202 @@
-// #include<stdio.h>
-// #include<stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-// ************ passing structure member as argument ********************
+// ****************** creating of a node **************
 
-// struct student
-// {
-//     char name[50];
-//     int age;
-//     int roll_no;
-//     float marks;
-// };
-
-// void print(char name[], int age, int roll_no, float marks)
-// {
-//     printf("%s %d %d %.2f", name, age, roll_no, marks);
-// }
+struct node
+{
+    int data;
+    struct node *link;
+};
 
 // int main()
 // {
-//     struct student s1 = {"manish", 21, 12, 75.63};  // passing value into the structure
-//     print(s1.name, s1.age, s1.roll_no, s1.marks);   // passing structure member as an argument
-// }
+//     struct node *head = NULL;
 
+//     head = (struct node *)malloc(sizeof(struct node));
 
+//     head->data = 45;
+//     head->link =NULL;
 
-
-//  now lets pass the argument as address to structure i.e. call by reference
-
-// struct charset{
-//     char s;
-//     int i;
-// };
-
-// void keyvalue(char *s, int *i)  // we know for holding the value of address we have pointer variable
-// {
-//     scanf("%c %d", s, i);
-// }
-
-// int main()
-// {
-//     struct charset cs;
-//     keyvalue(&cs.s, &cs.i);   // call by reference
-//     printf("%c %d", cs.s, cs.i);
+//     printf("%d", head->data);
 //     return 0;
 // }
 
 
 
-// ****************** passing structure variable as argument *********************
 
-// struct point
+// ************** creating a complete link list ***************
+
+// struct node
 // {
-//     int x;
-//     int y;
-// };
-
-// void print(struct point p)
-// {
-//     printf("%d %d\n", p.x, p.y);
-// }
-
-// int main()
-// {
-//     struct point p1 = {23, 45};
-//     struct point p2 = {56, 90};
-//     print(p1);
-//     print(p2);
-//     return 0;
-// }
-
-
-
-// *************** passing address of the structure **************
-
-// if the size of the structure is very large then passing the copy
-// of whole structure is not efficient
-// better choice: pass the address of the structure
-// and use the arrow operator(->) to access the structure members inside the called function
-
-// struct point
-// {
-//     int x;
-//     int y;
-// };
-
-// void print(struct point *ptr)
-// {
-//     printf("%d %d\n", ptr->x, ptr->y);
-// }
-
-// int main()
-// {
-//     struct point p1 = {23, 45};
-//     struct point p2 = {56, 90};
-
-//     print(&p1);
-//     print(&p2);
-//     return 0;
-// }
-
-
-
-// ***************** returning a structure variable from the function *****************
-
-// struct point
-// {
-//     int x;
-//     int y;
-// };
-
-// struct point edit(struct point p)
-// {
-//     p.x = p.x + 1;
-//     p.y = p.y + 5;
-//     return p;
-// }
-
-// void print(struct point p)
-// {
-//     printf("%d %d\n", p.x, p.y);
-// }
-
-// int main()
-// {
-//     struct point p1 = {23, 45};
-//     struct point p2 = {56, 90};
-//     p1 = edit(p1);
-//     p2 = edit(p2);
-//     print(p1);
-//     print(p2);
-//     return 0;
-// }
-
-
-
-// ******************** returning a pointer to a structure from the function ****************
-
-// struct point
-// {
-//     int x;
-//     int y;
-// };
-
-// struct point* edit(int a, int b)
-// {
-//     struct point *ptr = (struct point *)malloc(sizeof(struct point));
-//     ptr->x = a;
-//     ptr->y = b + 5;
-//     return ptr;
-// }
-
-// void print(struct point *p)
-// {
-//     printf("%d %d\n", p->x, p->y);
-// }
-
-// int main()
-// {
-//     struct point *p1, *p2;
-//     p1 = edit(23, 45);
-//     p2 = edit(56, 90);
-//     print(p1);
-//     print(p2);
-//     free(p1);
-//     free(p2);
-//     return 0;
-// }
-
-
-// ********************** passing array of structure as argument ***********************
-
-// struct abc
-// {
-//     int a;
-//     int b;
-// };
-
-// void print(struct abc arr[])
-// {
-//     for(int i=0;i<2;i++)
-//     {
-//         printf("%d %d\n", arr[i].a, arr[i].b);
-//     }
-// }
-
-// int main()
-// {
-//     struct abc arr[2] = {{1,2}, {3,4}};
-//     print(arr);
-//     return 0;
-// }
-
-
-
-// ************** what are self referential structures ******************
-
-// self referential structures are those structures in which one or more pointers points to the 
-// structure of the same type
-
-// struct code
-// {
-//     int i;
-//     int c;
-//     struct code *ptr;
+//     int data;
+//     struct node *link;
 // };
 
 // int main()
 // {
-//     struct code var1;
-//     struct code var2;
+//     struct node *head = NULL;  // it is good practice to initialise pointer with a Null value
+    
+//     head = malloc(sizeof(struct node));  // no need type cast in cast
+//     head->data = 45;
+//     head->link = NULL;
 
-//     var1.i = 65;
-//     var1.c = 'A';
-//     var1.ptr = NULL;
+//     struct node *current = NULL;
+//     current = malloc(sizeof(struct node));
+//     current->data = 98;
+//     current->link = NULL;
 
-//     var2.i = 66;
-//     var2.c = 'B';
-//     var2.ptr = NULL;
+//     // till now there is not any connection between head node and current node
+//     // now lets establish that connection
+//     // and for making that connection we need to update the link part of head
 
-//     // now let me establish the connection between var1 and var2
-//     var1.ptr = &var2;
-//     // var1.ptr is holding the address of var2 so
-//     // now var1.ptr can access the element of var2
-//     // its use in linked list
-//     printf("%d %c", var1.ptr->i, var1.ptr->c);
+//     head->link = current;
 //     return 0;
+
 // }
+
+
+
+
+// ***************** creating link list of three node **************
+// see we have two method to do this
+// first is just create a third pointer and connect with the second node
+// second method is just moving the current node
+// lets see
+
+// struct node
+// {
+//     int data;
+//     struct node *link;
+// };
+
+
+
+// ****************** counting of node **********************
+
+void count_of_nodes(struct node *head)
+{
+    int count = 0;
+    if(head == NULL)
+    {
+        printf("Linked List is empty");
+    }
+    struct node *ptr = NULL;
+    ptr = head;
+    while (ptr != NULL)
+    {
+        count++;
+        ptr = ptr->link;
+    }
+    printf("\ntotal node in the link list is : %d", count);
+
+}
+
+
+void print_data(struct node *head)
+{
+    if(head == NULL)
+    {
+        printf("Link list is empty");
+    }
+
+    struct node *ptr = NULL;
+    ptr = head;
+    while (ptr!=NULL)
+    {               
+        printf("data is %d: \n", ptr->data);
+        ptr = ptr->link;
+    }
+    
+}
+
+void printing_all_node(struct node *head)
+{
+    if(head == NULL)
+    {
+        printf("Link list is empty");
+    }
+    int count = 0;
+    printf("head containing the address of first node i.e: %d\n", head);
+    struct node *ptr = NULL;
+    ptr = head;
+    while(ptr!=NULL)
+    {   
+        count++;
+        printf("address of node %d is %d and it containing the data %d and address of next node(its link part) is %d\n",count,ptr, ptr->data, ptr->link);
+        ptr = ptr->link;
+    }
+
+}
+
+void insert_node_at_end(struct node *head)
+{
+    if(head == NULL)
+    {
+        printf("linked list is empty");
+    }
+    struct node *ptr = NULL;
+    struct node *tmp = NULL;
+    ptr = head;
+
+    
+    while(ptr!=NULL)
+    {
+        tmp = ptr;
+        ptr = ptr->link;
+    }
+    printf("\naddress of last node before inserting the node at last: %d",tmp);
+    printf("\nlink part of last node(i.e address to the next node which is not available) is %d: ",tmp->link);
+
+
+    printf("\nenter the data you want to insert at the end of the list: ");
+    int ldata;
+    scanf("%d", &ldata);
+
+    // now lets create a node for last node
+
+    struct node *last_node = malloc(sizeof(struct node));
+    printf("\naddress of the inserted node at the last(last node address) is: %d", last_node);
+    tmp->link = last_node;
+    last_node->link = NULL;
+    last_node->data = ldata;
+
+}
+
+
+int main()
+{
+    struct node *head = malloc(sizeof(struct node));
+    head->data = 45;
+    head->link = NULL;
+
+    struct node *current = malloc(sizeof(struct node));
+    current->data = 98;
+    current->link = NULL;
+    head->link = current;
+
+    current = malloc(sizeof(struct node));  // yah i can use the current poiter varaible again, it can move but  i have to give the size of memory once again
+    current->data = 3;
+    current->link = NULL;
+
+    head->link->link = current;
+
+    // now lets call from here to adding the last node
+
+    printf("\nhow many node you want to insert at the end: ");
+    int num;
+    scanf("%d", &num);
+
+    for(int i=0;i<num;i++)
+    {
+        insert_node_at_end(head);
+    }
+
+    count_of_nodes(head);
+    printf("\n");
+    print_data(head);
+    printf("\n");
+    printing_all_node(head);
+
+    return 0;
+}
